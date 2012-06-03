ac879d21d86f68e9b2adaac0f7ecbb32a54b5d25
David Adsit <david.adsit@gmail.com>
2012-06-03 14:56:00 -0600

    added more details to the samples

--- test/learning-the-tools-test.js
+++ test/learning-the-tools-test.js
@@ -1,13 +1,21 @@
 var assert = buster.assert;
 
 var foo = {
+
   method1: function () {
     console.log("method1 called")
     return "method1 result"
+  },
+
+  method2: function (x) {
+    console.log("method2 called with " + x)
+    return "method2 result " + x
   }
+
 }
 
 buster.testCase("learning about buster and sinon", {
+
   setUp: function () {
   },
 
@@ -49,6 +57,26 @@ buster.testCase("learning about buster and sinon", {
 
     console.log("calling the real method: foo.method1()")
     assert.equals("method1 result", foo.method1())
+
+    //Look at the console window in your browser.
+  },
+
+  "mocks can verify args": function () {
+    buster.assert(true) // work around for buster bug that affects sinon mocks
+    var mockFoo = sinon.mock(foo)
+
+    mockFoo.expects("method2").withArgs("my arg").once()
+
+    console.log("calling the mocked method: foo.method2('my arg')")
+    foo.method2("my arg")
+
+    mockFoo.verify()
+  
+    console.log("restoring the mocked object: foo")
+    mockFoo.restore()
+
+    console.log("calling the real method: foo.method2('other arg')")
+    assert.equals("method2 result other arg", foo.method2("other arg"))
 
     //Look at the console window in your browser.
   }
