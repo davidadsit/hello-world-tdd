1073712210034086dfd4b52c9fbdfa93e9e60f40
David Adsit <david.adsit@gmail.com>
2012-06-05 20:25:28 -0600

    first test fails

--- test/hello-world-test.js
+++ test/hello-world-test.js
@@ -2,8 +2,15 @@ var assert = buster.assert;
 
 buster.testCase("hello world", {
 
-  "name of the test": function () {
-    
+  "send hello world to the printer": function () {
+    buster.assert(true) // buster bug
+    var mockPrinter = sinon.mock(printer)
+    mockPrinter.expects("print").withArgs("hello world").once()
+
+    helloWorld.greet()
+
+    mockPrinter.verify()
+    mockPrinter.restore()
   }
 
 });
