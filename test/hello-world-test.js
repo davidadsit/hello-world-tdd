4109c0cc0206b58211c01550177ef4ec3ffedc8f
David Adsit <david.adsit@gmail.com>
2012-06-05 20:45:13 -0600

    Test 2 fails for the expected reason

--- test/hello-world-test.js
+++ test/hello-world-test.js
@@ -18,7 +18,14 @@ buster.testCase("hello world", {
 buster.testCase("printer tests", {
   
   "print sends the message to console.log": function () {
+    buster.assert(true) // buster bug
+    var mockConsole = sinon.mock(console)
+    mockConsole.expects("log").withArgs("test message").once()
 
+    printer.print("test message")
+
+    mockConsole.verify()
+    mockConsole.restore()
   }
 
 });
